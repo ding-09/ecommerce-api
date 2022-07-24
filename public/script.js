2c321431e357a163ff444a7ccc80a27f527f1baa
Tiffany <yang.ding@hotmail.com>
2022-07-24 14:17:19 -0700

    added tooltip and tooltip animation for when copy icon is clicked

--- public/script.js
+++ public/script.js
@@ -7,5 +7,13 @@ copyBtns.forEach((copyBtn) =>
     const pre = e.currentTarget.previousElementSibling;
     const codeBlock = pre.firstChild.innerText;
     navigator.clipboard.writeText(codeBlock);
+
+    // toggle tooltip after copy icon has been clicked
+    const tooltip = e.currentTarget.nextElementSibling;
+    tooltip.classList.add('tooltip-animation');
+
+    setTimeout(() => {
+      tooltip.classList.remove('tooltip-animation');
+    }, 1500);
   })
 );
